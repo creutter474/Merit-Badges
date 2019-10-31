c980a384daede421e83dc63e82d45c1a1d0784d2
creutter474 <54862349+creutter474@users.noreply.github.com>
2019-10-31 14:04:37 -0400

    Create 2-guess-validate.js

---
+++ guess/2-guess-validate.js
@@ -0,0 +1,26 @@
+/* Guess Game: valadator function: merit badge 2
+ * adds counting turns, quit with q, and validating guess
+ * By Connor Reutter
+ */
+
+var answer = Math.floor(Math.random()*100)+1;
+console.log(answer);
+var guess = 0;
+var turns = 0;
+while(guess != answer){
+guess = prompt("Guess my number 1-100");
+	if (guess == q) break;
+	if (validator(guess)==true){
+	turns++;
+if (guess<answer) alert("too low try again");
+else if (guess>answer) alert("Too high try again");
+  }
+  else alert("Invalid guess");
+}
+// Alert that they got it. 
+ if (guess == answer) alert("You got it in ("+turns+") turns!");
+ else alert("Quitter!");
+ function validator(guess) {
+	 if (guess >0 && guess >101) validator true;
+	 else validator false;
+ }
